a58eb514f2bcb7e92b0cd70fa01214b85e12639e
CarterChilds <carterchilds@live.com>
2018-04-03 15:56:39 -0600

    worked on nav-bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './Nav.css';
+import flickrLogo from './flickrLogo.svg'
+import {Link} from 'react-router-dom';
 
 export default function Nav (){
     return(
         <div className='Nav'>
             <div className='primaryNav'>
-            <a className='login' href='http://localhost:8181/auth'>Login</a>
+                <div className='logo-container'>
+            <img className='logo' src={flickrLogo}/>
+                </div>
+            <Link to='/profile'><span className='you'>You</span></Link>
+            <Link to='/dashboard'><span className='photostream'>Photostream</span></Link>
+            <Link to='/upload'><span className='create'>Create</span></Link>
+            <div className='subNav'>
+            <span className='search'>
+                <input className='search-child' type='text' placeholder='Search'/>
+            </span>
 
+        <a className='login' href='http://localhost:8181/auth'>Login</a>
             </div>
+        </div>
         </div>
     )
 }
